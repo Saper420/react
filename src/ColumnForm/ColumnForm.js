14e307c4ec09984df1105be06b117f50c8e0ad8c
Michał Stankowski <monster26k@gmail.com>
2022-01-20 19:39:25 +0100

    new features for site

---
+++ src/ColumnForm/ColumnForm.js
@@ -0,0 +1,25 @@
+import styles from './ColumnForm.module.scss';
+import { useState } from 'react';
+import TextInput from '../TextInput/TextInput';
+import Button from '../Button/Button';
+
+const ColumnForm = props => {
+    const [title, setValueTitle] = useState('');
+    const [icon, setValueIcon] = useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
+        props.action({ title: title , icon: icon});
+        setValueTitle('');
+        setValueIcon('');
+    };
+
+	return (
+        <form onSubmit={handleSubmit} className={styles.columnForm}>
+            <span>Title:</span><TextInput value={title} onChange={e => setValueTitle(e.target.value)} /> 
+            <span>Icon:</span><TextInput value={icon} onChange={e => setValueIcon(e.target.value)} />
+            <Button>add column</Button>
+        </form>
+	);
+};
+
+export default ColumnForm;
